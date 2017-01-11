7385ad5392c1e53a97c6658ccab02de574541f8c
sunan <sunanchn@126.com>
2017-01-11 20:58:29 +0800

    设置主页排序

--- app/models/job.rb
+++ app/models/job.rb
@@ -13,5 +13,8 @@ class Job < ActiveRecord::Base
     self.is_hidden = true
     self.save
   end
- 
+
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
+
 end
